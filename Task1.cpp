fbc11cb6ab05644409e07b5e93fadfc090eab090
Vitalii Vlasov TCA-143 <vitalij1663@gmail.com>
2025-03-05 18:47:21 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 double getA(const double x, const double y, const double z);
+double getB(const double x, const double y, const double z);
 
 
 int main() {
@@ -13,20 +14,27 @@ int main() {
   double y = 1.7;
   double z = 0.44;
 
-  // Вычисление b
-  double b = y * sin(a * z * z * cos(2 * z)) - 1;
+  
+  
 
 
   // Вывод результатов (с заданной точностью)
   cout << "\nРезультаты вычислений:" << endl;
   cout << fixed << setprecision(10); // Устанавливаем точность вывода до 10 знаков после запятой
   cout << "a = " << getA(x, y, z) << endl;
-  cout << "b = " << b << endl;
+  cout << "b = " << getB(x, y, z) << endl;
 
   return 0;
 }
 
-double getA(const double x, const double y, const double z)
+double getA(const double x, const double y, const double z);
 {
     return exp(-y * z) * sin(x * z - y) - sqrt(y * z + x);
+}
+}
+
+double getB(const double x, const double y, const double z);
+{
+   return y * sin(a * z * z * cos(2 * z)) - 1;
+}
 }
