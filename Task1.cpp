0e5204a3c36921a1a2d4c753f2507845d0eb5938
Vitalii Vlasov TCA-143 <vitalij1663@gmail.com>
2025-05-14 18:11:44 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 #include <math.h>
 
-// Вариант 4
-// а = ⅇ ^ (-yz)⋅sin⁡〖(xz - y)〗 - √(| yz + x | )
-// b = y sin⁡(az ^ 2 cos⁡2z) - 1
-
-// float - тип данных, который хранят дробные числа
-// double - тип данных, который хранят дробные числа
 
 using namespace std;
+/**
+ *@brief - Функция для вычисления A по заданному уравнению 
+ *@param x - значение переменной x
+ *@param y - значение переменной y
+ *@param z - значение переменной z
+ *@return - возвращает рассчитанное значение
+ */
+ 
+double getA(const double x, const double y, const double z);
+/**
+ *@brief - Функция для вычисления B по заданному уравнению 
+ *@param x - значение переменной x
+ *@param y - значение переменной y
+ *@param z - значение переменной z
+ *@return - возвращает рассчитанное значение
+ */
+
+double getB(const double x, const double y, const double z);
 
 int main()
 {
-  // Смена кодировки для вывода русского языка
-  setlocale(LC_ALL, "Russian");
+  const double z = 0.44; // константа по условию задачи
+  const double x = -0.5; // константа по условию задачи
+  const double y = 1.7; // константа по условию задачи
+  cout<<"A = "<<getA(x,y,z)<<endl;
+  cout<<"B = "<<getB(x,y,z);
+  return 0;
+}
+ double getA(const double x, const double y, const double z)
+{
 
-  float a, b; // переменные, содержащие значения функций
-  const float z = 0.44; // константа по условию задачи
-  const float x = -0.5; // константа по условию задачи
-  const float y = 1.7; // константа по условию задачи
+  return exp(-y * z) * sin(x * z - y) - sqrt(abs(y * z + x));
+}
+ double getB(const double x, const double y, const double z)
+{
 
-  a = exp(-y * z) * sin(x * z - y) - sqrt(abs(y * z + x));
-  b = y * sin(y * pow(z, 2) * cos(2 * z)) - 1;
-
-  cout << "Функция a: " << a << endl;
-  cout << "Функция b: " << b << endl;
+ return y * sin(y * pow(z, 2) * cos(2 * z)) - 1;
 
 }
