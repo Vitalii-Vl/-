e12ce4f6f4e45451049e1b7e00826c480b112512
Vitalii Vlasov TCA-143 <vitalij1663@gmail.com>
2025-06-12 16:33:31 +0300

    Update Задача 4.cpp

--- Задача 4.cpp
+++ Задача 4.cpp
@@ -8,14 +8,12 @@ using namespace std;
 /**
  * @brief Считывает значение типа double с клавиатуры с проверкой на корректность ввода.
  * @return Введенное значение типа double.
- * @throws std::runtime_error Если ввод некорректен.
  */
 double getValue();
 
 /**
  * @brief Проверяет, является ли шаг положительным.
  * @param step Шаг.
- * @throws std::runtime_error Если шаг не является положительным.
  */
 void checkStep(const double step);
 
@@ -23,59 +21,71 @@ void checkStep(const double step);
  * @brief Вычисляет значение функции y(x) = sin(log(x)) - cos(log(x)) + 2*log(x).
  * @param x Аргумент функции.
  * @return Значение функции y(x).
- * @throws std::domain_error Если x не входит в область определения функции (x <= 0).
  */
 double getY(const double x);
+
+/**
+ * @brief Цикл вычисления y(x) от x_start до x_end с шагом delta с проверкой ln(x)
+ * @param x_start – начальное значение
+ * @param x_end – конечное значение
+ * @param step – шаг изменения x
+ */
+void computeAndPrint(const double x_start, const double x_end, const double step);
 
 /**
  * @brief Главная функция программы.
  * @return 0, если программа выполнена корректно, иначе 1
  */
-int main() {
+int main() 
+{
+    std::cout << "Enter xn: ";
+    double xstart = getValue();
 
-  
-        cout <<"Enter xn: "
-       double xstart = getValue();
+    std::cout << "Enter xk: ";
+    double xend = getValue();
 
-        cout <<"Enter xk: ";
-       double xend = getValue();
+    std::cout << "Enter step: ";
+    double step = getValue();
 
-        cout <<"Enter step: ";
-       double step = getValue();
+    checkStep(step);
 
-        checkStep(step);
-
-        for (double x = xstart; x <= xend+step; x += step) { 
-                cout <<"x = " <<x <<" y = " <<getY(x) <<endl;
-
+    computeAndPrint(xstart, xend, step);
     return 0;
 }
+
 
 double getValue() {
     double value = 0;
     cin >> value;
 
     if (cin.fail()) {
-        cin.clear(); 
+        cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Incorrect value" << endl;
-        abort(); 
+        std::cout << "Incorrect value";
+        abort();
     }
-
     return value;
 }
 
 void checkStep(const double step) {
     if (step <= 0) {
-        cout << "Error value" << endl;
+        std::cout << "Error value";
         abort();
     }
 }
 
-double getY(const double x) {
-    if (x <= 0) {
-        cout << "X must be more then 0" << endl;
-        abort();
-    }
+double getY(const double x){
     return sin(log(x)) - cos(log(x)) + 2 * log(x);
+}
+
+void computeAndPrint(const double x_start, const double x_end, const double delta)
+{
+    cout << "\nРезультаты:\n";
+    for (double x = x_start; x <= x_end; x += delta) {
+        if (x <= 0.0) {
+            cout << "Пропущено x=" << x << ": вне области определения ln(x)\n";
+            continue;
+        }
+        cout << "y(" << x << ") = " << getY(x) << endl;
+    }
 }
